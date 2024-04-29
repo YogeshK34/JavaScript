d2591c679b63adfa44db86ca9952d4db7ae3be59
yogeshk34 <khutwadyogesh34@gmail.com>
2024-04-29 17:13:06 +0530

    Added Callback,Functions & Loops

---
+++ Callback Functions/Program1.js
@@ -0,0 +1,1 @@
+// Program to show an example of callback functiom
---
+++ Loops/SumofNumbers.js
@@ -0,0 +1,5 @@
+var sum = 0;
+for (let i = 0; i <= 500; i++) {
+    sum = sum + i;
+}
+console.log("Sum of numbers from 0 to 500 is : " ,sum);
