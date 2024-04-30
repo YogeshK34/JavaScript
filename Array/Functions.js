c2a9fb74660aaf14208b9f3e688d827ad483e84c
yogeshk34 <khutwadyogesh34@gmail.com>
2024-04-30 21:24:50 +0530

    added Array function

---
+++ Array/Functions.js
@@ -0,0 +1,35 @@
+// Array handbook
+
+// Array:   push(), pop(), shift(), unshift(), splice(), slice(),
+// concat(), forEach(), map(), filter(), reduce(), find(), sort()
+
+
+
+// // Array push()
+// let arr = [11,89,64,83,15,67];
+// let push = arr.push(45.78,564);
+// console.log(arr)
+
+
+// // Array pop
+// let arr1 = [11,89,64,83,15,67];
+// let pop = arr1.pop();
+// console.log(arr1)
+
+//Array shift()
+// let arr2 = [11,89,64,83,15,67];
+// let shift = arr2.shift();
+// console.log(arr2)
+// console.log(shift)
+
+// // Array unshift()
+// let arr3 = [11, 89, 64, 83, 15, 67];
+// let shift1 = arr3.unshift(14);
+// console.log(arr3);
+// console.log(shift1);
+
+// Array splice()
+let arr4 = [11, 89, 64, 83, 15, 67];
+let splice = arr4.splice(1,5,91,92,93,94,95);
+console.log(arr4);
+console.log(splice);
