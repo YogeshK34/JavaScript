855593ae1166c1a0ce308175e8b6ec754bf831ef
yogeshk34 <khutwadyogesh34@gmail.com>
2024-04-30 21:03:55 +0530

    created numbers folder and added parseInt & parseFloat

---
+++ Numbers/ParseFloat.js
@@ -0,0 +1,14 @@
+// Javascript program to display parsefloat function
+let num = "10.5";
+
+// Displays 10.5
+console.log(parseFloat(num));
+
+// Displays 10
+
+console.log(parseFloat(num).toFixed(0));
+
+// Displays 11
+console.log(parseFloat(num).toFixed(1));
+
+// Displays 10.500000000000001
---
+++ Numbers/ParseInt.js
@@ -0,0 +1,11 @@
+// // Program to show parseInt function
+// let num = parseInt("123"); // Returns 123
+// let hex = parseInt("1A", 16); // Parses "1A" as a hexadecimal number, returns 26
+// let invalid = parseInt("hello"); // Returns NaN since "hello" cannot be converted to a number
+
+// console.log(num);
+// console.log(hex);
+// console.log(invalid);
+
+let variable = parseInt("22",3)
+console.log(variable)
